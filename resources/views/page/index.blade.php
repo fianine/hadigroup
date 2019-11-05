6ca59e9972bdbef05ac07add91fece613c1e8e8b
fianine <princealfiyana@gmail.com>
2019-11-05 13:54:27 +0000

    company section

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -26,7 +26,7 @@
           <h1>About Us</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-          <a href="#" class="co-3">Read More</a>
+          <a href="#" class="co-3">Read More <i class="fa fa-long-arrow-right"></i> </a>
         </div>
       </div>
     </div>
@@ -40,25 +40,38 @@
             <p style="text-align: center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
         </div>
+        <div class="br-company"></div>
         <div class="col-md-3">
           <img class="img-center-page-three" src="{{ asset('/assets/frontend/img/hamura.png') }}">
           <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#" class="text-center co-3" style="display: block;"> Read More </a>
+          <a href="#" class="text-center co-3" style="display: block;"> Read More <i class="fa fa-long-arrow-right"></i> </a>
         </div>
         <div class="col-md-3">
           <img class="img-center-page-three" src="{{ asset('/assets/frontend/img/gmt.png') }}">
           <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#" class="text-center co-3" style="display: block;"> Read More </a>
+          <a href="#" class="text-center co-3" style="display: block;"> Read More <i class="fa fa-long-arrow-right"></i> </a>
         </div>
         <div class="col-md-3">
           <img class="img-center-page-three" src="{{ asset('/assets/frontend/img/didaya.png') }}">
           <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#" class="text-center co-3" style="display: block;"> Read More </a>
+          <a href="#" class="text-center co-3" style="display: block;"> Read More <i class="fa fa-long-arrow-right"></i> </a>
         </div>
         <div class="col-md-3">
           <img class="img-center-page-three" src="{{ asset('/assets/frontend/img/bengkelhost.png') }}">
           <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="#" class="text-center co-3" style="display: block;"> Read More </a>
+          <a href="#" class="text-center co-3" style="display: block;"> Read More <i class="fa fa-long-arrow-right"></i> </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="page-four">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="company-header mx-auto">
+            <h1 style="text-align: center">Blog</h1>
+            <p style="text-align: center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br> Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
         </div>
       </div>
     </div>
--- resources/views/page/layouts/app.blade.php
+++ resources/views/page/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('/assets/frontend/css/style.css') }}">
   </head>
 
